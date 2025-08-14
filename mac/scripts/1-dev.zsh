6a78f10b30d4df4a228df67a64de38e138521097
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-08-14 08:46:30 +0200

    added protobuf install

--- mac/scripts/1-dev.zsh
+++ mac/scripts/1-dev.zsh
@@ -4,9 +4,17 @@ set -e  # exit script if a command fails
 
 brew update
 
+echo ""
+echo "starting install..."
+
 brew install btop curl bat fd
 brew install git ripgrep jq tldr
 brew install wget
 
 brew install fzf
 $(brew --prefix)/opt/fzf/install --bin #downloads fzf binaries, but does not generate .fzf/zsh
+
+brew install protobuf
+protoc --version
+
+echo "done dev install"
