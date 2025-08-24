45481d4d9cb3d844292257fd0354d03b323f6958
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-08-24 09:23:46 +0200

    updated global git config

--- mac/scripts/git-config.zsh
+++ mac/scripts/git-config.zsh
@@ -1,27 +1,25 @@
 #!/bin/zsh
 
 # Check if user.name and user.email are already set globally
-existing_name=$(git config --global user.name)
 existing_email=$(git config --global user.email)
 
-if [[ -n "$existing_name" && -n "$existing_email" ]]; then
-  echo "Git user.name and user.email are already set:"
-  echo "  user.name: $existing_name"
-  echo "  user.email: $existing_email"
-  echo "Skipping Git configuration."
-  exit 0
-fi
+#Set or update user.name
+git config --global user.name "Torkel Aannestad"
 
-read -r "git_user?Enter your Git user.name: "
-read -r "git_email?Enter your Git user.email: "
-
-git config --global user.name "$git_user"
-git config --global user.email "$git_email"
-
-# Set some useful Git global configs
-git config --global core.editor "zed --wait"     #--wait waits for zed to close
+# Set or update some git options
+git config --global core.editor "vim"     #zed --wait waits for zed/vs code to close
 git config --global pull.rebase true
 git config --global init.defaultBranch main
 git config --global credential.helper osxkeychain
+
+#If email not in config, read from input (to not include email in repo)
+if [[ -n "$existing_email" ]]; then
+  echo "Git user.email is already set"
+  echo "  user.email: $existing_email"
+  echo "Skipping updating user.email"
+  exit 0
+fi
+read -r "git_email?Enter your Git user.email: "
+git config --global user.email "$git_email"
 
 echo "Git setup is complete."
