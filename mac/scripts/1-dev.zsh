87c6bd5d808992a5a169f6ebd04e7dddf92acebb
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-08-28 08:31:51 +0200

    added lazygit

--- mac/scripts/1-dev.zsh
+++ mac/scripts/1-dev.zsh
@@ -10,6 +10,7 @@ echo "starting install..."
 brew install btop curl bat fd
 brew install git ripgrep jq tldr
 brew install wget
+brew install lazygit
 
 brew install fzf
 $(brew --prefix)/opt/fzf/install --bin #downloads fzf binaries, but does not generate .fzf/zsh
