42aff46e3d5ab30677529796cd2953b365a8ba1b
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-08-31 11:47:40 +0200

    linux: added lines to js install

--- linux/scripts/javascript.sh
+++ linux/scripts/javascript.sh
@@ -2,8 +2,15 @@
 
 set -e  # exit script if a command fails
 
+echo ""
+echo "node"
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 npm config set prefix ~/.local/npm #zshrc should include export PATH="$HOME/.local/npm/bin:$PATH"
 
+echo ""
+echo "deno"
 curl -fsSL https://deno.land/install.sh | sh
+
+echo ""
+echo "bun"
 curl -fsSL https://bun.sh/install | bash
